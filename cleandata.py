9378cf1dff02339d49154d42554767f1350dfea9
Ing3567 <phongphol.suksomphong@g.swu.ac.th>
2025-03-19 09:06:11 +0700

    modified cleandata

--- cleandata.py
+++ cleandata.py
@@ -9,17 +9,7 @@ def cleanGender(gender):
         return gender
 
 def CleanAge(data):
-    # คำนวณ IQR (Interquartile Range)
-    Q1 = data['Age'].quantile(0.25)  # ควอไทล์ที่ 1 (Q1)
-    Q3 = data['Age'].quantile(0.75)  # ควอไทล์ที่ 3 (Q3)
-    IQR = Q3 - Q1
-
-    # หาขอบเขตล่างและบน
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-
-    # กำจัดค่านอกเกณฑ์
-    data = data[(data['Age'] >= lower_bound) & (data['Age'] <= upper_bound)]  # fix this line
+    data = data[(data['Age'] >= 0) & (data['Age'] <= 100)]  # fix this line
     return data
 
 def cleandata(data):
