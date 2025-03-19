5435c69d93f367ad32b6795214117681f87f8e62
Ing3567 <phongphol.suksomphong@g.swu.ac.th>
2025-03-19 10:23:56 +0700

    modified p-value

--- p-value.py
+++ p-value.py
@@ -5,16 +5,20 @@ from scipy.stats import chi2_contingency
 def p_value(data):
 
     y = input('Select You y-axis: ')
+
+    while(True):
+        if(y.upper()=='X'):
+            break
         
-    data = np.array([[20, 30, 50],
-                    [40, 10, 50]])
+        data = pd.crosstab(data['leave'], data[y])
 
-    
-    chi2, p, dof, expected = chi2_contingency(data)
+        
+        chi2, p, dof, expected = chi2_contingency(data)
 
-    
-    print("Chi-Square Value:", chi2)
-    print("p-value:", p)
-    print("Degrees of Freedom:", dof)
-    print("Expected Frequencies Table:")
-    print(expected)
+        
+        print("Chi-Square Value:", chi2)
+        print("p-value:", p)
+        print("Degrees of Freedom:", dof)
+        print("Expected Frequencies Table:")
+        print(expected)
+        print(f'Exit enter X')
